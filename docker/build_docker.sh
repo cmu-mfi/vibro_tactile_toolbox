6f3e315292a09a0b20652456f38d75ba7f76ab5b
Kevin Zhang <kevinleezhang@gmail.com>
2024-11-10 23:10:47 -0600

    Modified launch files and made them more modular.

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,2 +1,5 @@
-docker build -t noetic_realsense_and_sounddevice_ros -f Dockerfile.realsense_and_sounddevice_ros .
-docker build -t noetic_orbbec_ros -f Dockerfile.orbbec_ros .
+docker build -t realsense_ros:noetic -f Dockerfile.realsense_ros .
+docker build -t sounddevice_ros:noetic -f Dockerfile.sounddevice_ros .
+docker build -t orbbec_ros:noetic -f Dockerfile.orbbec_ros .
+docker build -t robotiq_mm_ros:noetic -f Dockerfile.robotiq_mm_ros .
+docker build -t vibro_tactile_toolbox:noetic -f Dockerfile.vibro_tactile_toolbox .
