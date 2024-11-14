751fea40e148bdc2369a70bb9ddc8dc6aafdfe5b
Kevin Zhang <kevinleezhang@gmail.com>
2024-11-13 20:26:17 -0600

    Using local version of vibro_tactile_toolbox.

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -4,4 +4,4 @@ docker build -t realsense_ros:noetic -f Dockerfile.realsense_ros .
 docker build -t sounddevice_ros:noetic -f Dockerfile.sounddevice_ros .
 docker build -t orbbec_ros:noetic -f Dockerfile.orbbec_ros .
 docker build -t robotiq_mm_ros:noetic -f Dockerfile.robotiq_mm_ros .
-docker build -t vibro_tactile_toolbox:noetic -f Dockerfile.vibro_tactile_toolbox .
+docker build -t vibro_tactile_toolbox_prereq:noetic -f Dockerfile.vibro_tactile_toolbox_prereq .
