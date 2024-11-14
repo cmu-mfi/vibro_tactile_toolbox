437613d6ad8b29ce024c9997d2c9965d540aa20c
Kevin Zhang <kevinleezhang@gmail.com>
2024-11-13 18:26:27 -0600

    Swapped from robot commander to robot controller.

--- scripts/save_lego_pose.py
+++ scripts/save_lego_pose.py
@@ -6,7 +6,7 @@ import numpy as np
 
 from geometry_msgs.msg import Pose, Twist
 
-from robot_controller.yk_commander import YaskawaRobotCommander
+from robot_controller.yk_controller import YaskawaRobotController
 
 from autolab_core import RigidTransform
 
@@ -17,11 +17,11 @@ def run():
     namespace = rospy.get_param("save_lego_pose/namespace")
     root_pwd = rospy.get_param("save_lego_pose/root_pwd")
 
-    robot_mg = YaskawaRobotCommander(namespace)
+    robot_commander = YaskawaRobotController(namespace)
     
     T_lego_ee = RigidTransform.load(root_pwd+'/transforms/lego_ee.tf')
 
-    T_ee_world = RigidTransform.from_pose_msg(robot_mg.get_current_pose(), from_frame='ee')
+    T_ee_world = RigidTransform.from_pose_msg(robot_commander.get_current_pose(), from_frame='ee')
 
     T_lego_world = T_ee_world * T_lego_ee
 
