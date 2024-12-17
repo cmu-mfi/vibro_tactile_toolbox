14de8f89d79e9a0ddfe7d77423b3e1ee93ba4ab5
Henrik <henrik.bingemann@arminstitute.org>
2024-12-17 15:30:19 -0500

    systems check with `exec -it run`

---
+++ convenience_scripts/systems_check.sh
@@ -0,0 +1,1 @@
+docker exec -it vibro_tactile_toolbox_noetic bash -c "source /ros1_ws/devel/setup.bash && cd /home/rtc_repos/vibro_tactile_toolbox/ && python tests/test_system.py -t nist -n yk_arm_rtc
